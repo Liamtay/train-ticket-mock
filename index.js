8cd046e6a5f4f7bf9b9a06568085a7d10c7539fc
陈晓斌 <465120558@qq.com>
2020-09-01 09:18:39 +0800

    feat：新增城市列表接口

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const { response } = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.status(200);
-    response.send('hello express');
-    response.end();
+  response.status(200);
+  response.send('hello express');
+  response.end();
 });
 
-app.get('/rest',(request,response)=>{
+app.get('/rest', (request, response) => {
   response.json({
-      name:'liam',
-      age:'26'
+    name: 'liam',
+    age: '26'
   })
 });
 
-app.listen(4000);
+app.get('/rest/cities', (request, response) => {
+  response.json({
+    cityList: [
+      { citys: [{ name: '鹌鹑' }, { name: '嗷嗷' }], title: 'A' },
+      { citys: [{ name: '宝宝' }, { name: '拜拜' }], title: 'B' },
+      { citys: [{ name: '宝宝' }, { name: '拜拜' },{ name: '宝宝' }, { name: '拜拜' },{ name: '宝宝' }, { name: '拜拜' },{ name: '宝宝' }, { name: '拜拜' },{ name: '宝宝' }, { name: '拜拜' },{ name: '宝宝' }, { name: '拜拜' },], title: 'C' },
+      { citys: [{ name: '南京' }], title: 'N' },
+      { citys: [{ name: '厦门' }], title: 'X' },
+    ]
+  })
+});
+
+app.listen(5000);
